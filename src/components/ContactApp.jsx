340f1db00f55b1031e2577620b3974b0a1f29f00
Calvin Kamtoso <calvinkmts@gmail.com>
2024-03-03 08:21:42 +0700

    feat: adding register page

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -3,23 +3,51 @@ import Navigation from "./Navigation.jsx";
 import HomePage from "../pages/HomePage.jsx";
 import AddPage from "../pages/AddPage.jsx";
 import {Route, Routes} from "react-router-dom";
+import RegisterPage from "../pages/RegisterPage.jsx";
 
-function ContactApp() {
+class ContactApp extends React.Component {
 
-    return (
-        <div className="contact-app">
-            <header className="contact-app__header">
-                <h1>Aplikasi Kontak</h1>
-                <Navigation/>
-            </header>
-            <main>
-                <Routes>
-                    <Route path="/" element={<HomePage/>}/>
-                    <Route path="/add" element={<AddPage/>}/>
-                </Routes>
-            </main>
-        </div>
-    );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            authedUser: null,
+        }
+    }
+
+    render() {
+
+        if (this.state.authedUser === null) {
+            return (
+                <div className="contact-app">
+                    <header className="contact-app__header">
+                        <h1>Aplikasi Kontak</h1>
+                    </header>
+                    <main>
+                        <Routes>
+                            <Route path="/*" element={<p>Halaman Login</p>}/>
+                            <Route path="/register" element={<RegisterPage/>}/>
+                        </Routes>
+                    </main>
+                </div>
+            );
+        }
+
+        return (
+            <div className="contact-app">
+                <header className="contact-app__header">
+                    <h1>Aplikasi Kontak</h1>
+                    <Navigation/>
+                </header>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/add" element={<AddPage/>}/>
+                    </Routes>
+                </main>
+            </div>
+        );
+    }
 }
 
 export default ContactApp;
